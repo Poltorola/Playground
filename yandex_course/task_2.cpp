d56a8c0def8df749a0d00e33c769f217b4952679
poltorola <poltorola@yandex.ru>
2020-10-09 12:39:53 +0300

    simple task made in gnu nano

---
+++ yandex_course/task_2.cpp
@@ -0,0 +1,23 @@
+// simple example of cast made in gnu nano 
+// haven't recieved clion license on my new laptop yet :(
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+	double n;
+
+	std::cin >> n;
+				//here comes nice green space)
+																					
+				
+				
+				
+				
+				
+				
+				
+	std::cout << n - (int)n << std:: endl;
+
+	return 0;
+}
